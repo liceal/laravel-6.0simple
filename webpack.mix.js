b150015be71f8f3085a57568eda6d7b98e638d46
liceal <675024132@qq.com>
2019-10-26 14:38:24 +0800

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,9 @@ const mix = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
+    js缓存 参数1:原来地址,参数2:保存位置
+    sass缓存 参数1:原来地址,参数2:保存位置
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
